a4a19953b4fa05d32aad5f7c131a63cbd2c18982
Oli Sturm <oliver+commitsigning@oliversturm.com>
2023-06-13 13:48:26 +0100

    migrate to vite

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import ProjectOverview from './ProjectOverview.jsx';
+
+function App() {
+  return <ProjectOverview />;
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+//import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 // Normally it's a good idea to use StrictMode because it helps
 // detect rendering issues in development. However, for this demo
 // we're deactivating it because in dev mode it causes additional
 // render cycles that muddy the waters for the purpose of this demo.
 
-root.render(
-  // <StrictMode>
-  <App />,
-  // </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  //  <React.StrictMode>
+  <App />
+  //  </React.StrictMode>,
 );
